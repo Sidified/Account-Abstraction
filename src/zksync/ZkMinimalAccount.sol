2cc6ff46f60f57011374e18e16a0595a519f41dd
Sid Harry <sidforwork46@gmail.com>
2026-04-02 23:43:16 +0530

    Implemented UserOp generation, signing, full ERC-4337 flow, and EVM-level debugging fixes (2)

--- src/zksync/ZkMinimalAccount.sol
+++ src/zksync/ZkMinimalAccount.sol
@@ -6,26 +6,30 @@ import {
     Transaction
 } from "lib/foundry-era-contracts/src/system-contracts/contracts/libraries/MemoryTransactionHelper.sol";
 
-contract ZkMinimalAccount is IAccount {
+abstract contract ZkMinimalAccount is IAccount {
     function validateTransaction(bytes32 _txHash, bytes32 _suggestedSignedHash, Transaction memory _transaction)
         external
         payable
+        virtual
         returns (bytes4 magic);
 
     function executeTransaction(bytes32 _txHash, bytes32 _suggestedSignedHash, Transaction memory _transaction)
         external
-        payable {}
+        payable
+        virtual {}
 
     // There is no point in providing possible signed hash in the `executeTransactionFromOutside` method,
     // since it typically should not be trusted.
-    function executeTransactionFromOutside(Transaction memory _transaction) external payable;
+    function executeTransactionFromOutside(Transaction memory _transaction) external payable virtual;
 
     function payForTransaction(bytes32 _txHash, bytes32 _suggestedSignedHash, Transaction memory _transaction)
         external
-        payable {}
+        payable
+        virtual {}
 
     function prepareForPaymaster(bytes32 _txHash, bytes32 _possibleSignedHash, Transaction memory _transaction)
         external
-        payable {}
+        payable
+        virtual {}
 }
 
